f7565109e1b0d3ad7bb87c902de2664b294b9f98
kglim <i7027@naver.com>
2022-03-11 12:46:08 +0900

    ecount class source2

--- FrameWorkETC/Ex01_Nullable/Program.cs
+++ FrameWorkETC/Ex01_Nullable/Program.cs
@@ -6,10 +6,78 @@ using System.Threading.Tasks;
 
 namespace Ex01_Nullable
 {
-    internal class Program
+    /*
+        System.Nullable
+        //값 타입 : int, long, double  원칙적으로 >> null (x)
+        //string ,  Car   >> null (0)
+        //string name = null  (참조타입)
+        //Car car = null      (참조타입)
+
+        //어느날
+        //DB 설계 : table column 타입 null여부 
+        create table emp (
+           empno int not null,
+           ename nvarchar(10) null,
+           age int null
+        );
+        insert into emp(empno, ename, age) values(7788,'홍길동',10);
+        >> 7788 / 홍길동 / 10
+        insert into emp(empno) values(7902);
+        >> 7902 / null / null
+        
+        class Emp {
+          int empno;
+          string ename;
+          int age;
+        } 
+        예외가 발생 ...
+        개발자 요구사항
+        
+        >> int age = null (안되는 것인데 .. 허용해 주세요)
+        >> 새로운 문법 >>  사용하면 >> null 허용(값타입)
+        >> int? i = null 가능
+    */
+    class Program
     {
         static void Main(string[] args)
         {
+            int? i = null;
+            Console.WriteLine("null 허용 : {0}",i);
+            double? j = 1.223546;
+            j = null;
+
+
+            int? i2 = null;
+            int j2;
+            //j2 = i2;  i2 가 null 가지고 있어요 안되요
+
+            //혼재 되어 있는 경우 사용법 1
+            if (i2 == null)
+            {
+                j2 = 0;
+            }
+            else {
+                j2 = (int)i2; //강제로 값타입 변환
+            }
+            Console.WriteLine(j2);
+
+
+            //혼재 되어 있는 경우 사용법 2
+            // ?? 연산자
+            // ?? 연산자 >>  null 이라면 오른쪽 값을 사용
+            int? i3 = null;
+           // i3 = 100;
+            int j3 = i3 ?? -1; //null 인 경우 -1 을 가지겠다
+            Console.WriteLine("j3 : {0}", j3);
+
+            int? i4 = null;
+            int? j4 = i4 + 100;
+            //과연 값은 : 
+            //프로그래밍 or DB 원칙적은 같은것
+            //**null 과의 모든 연산은 >> null (아무것도 없다)
+            Console.WriteLine("null 과의 모든 연산 : {0}", j4);
+
+
         }
     }
 }
---
+++ FrameWork_Collection/Ex01_ArrayList/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace Ex01_ArrayList
+{
+    class Program
+    {
+        public static void PrintValues(IEnumerable myList)
+        {
+            IList li = (IList)myList; //코드 
+            Console.WriteLine("count : {0}", li.Count);
+
+            foreach (Object obj in myList) {
+                Console.Write("   {0}", obj);
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            //Collection  구현하고 있는 (상속) 대표적인 클래스
+            //ArrayList 
+            //동적으로 데이터 관리 
+            //using System.Collections; 아래 있는 자원 
+
+            //ArrayList 내부적으로 데이터를 Array 관리
+
+            ArrayList list = new ArrayList();
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+
+
+            Console.WriteLine("ToString(): {0}",list.ToString());
+
+            foreach (int to in list)
+            {
+                Console.WriteLine("to : {0}",to);
+            }
+            Console.WriteLine("list.Count : {0}", list.Count); //java : list.size()
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine("list:[{0}]" , i);
+            }
+
+            Console.WriteLine("list ArrayList");
+            Console.WriteLine("Count : {0}", list.Count);
+            Console.WriteLine("Capacity : {0}", list.Capacity);
+
+
+
+            /////////////////////////////////////////////////////////////////
+            /// // Creates and initializes a new ArrayList.
+            ArrayList myAL = new ArrayList();
+            myAL.Add("The");
+            myAL.Add("quick");
+            myAL.Add("brown");
+            myAL.Add("fox");
+            myAL.Add("jumps");
+            myAL.Add("over");
+            myAL.Add("the");
+            myAL.Add("lazy");
+            myAL.Add("dog");
+
+            // Displays the ArrayList.
+            Console.WriteLine("The ArrayList initially contains the following:");
+            PrintValues(myAL);
+
+            //Removes the element containing "lazy".
+            myAL.Remove("lazy");
+
+            // Displays the current state of the ArrayList.
+            Console.WriteLine("After removing \"lazy\":");
+            PrintValues(myAL);
+
+            // Removes the element at index 5.
+            myAL.RemoveAt(5);
+
+            // Displays the current state of the ArrayList.
+            Console.WriteLine("After removing the element at index 5:");
+            PrintValues(myAL);
+
+            // Removes three elements starting at index 4.
+            myAL.RemoveRange(4, 3);
+
+            // Displays the current state of the ArrayList.
+            Console.WriteLine("After removing three elements starting at index 4:");
+            PrintValues(myAL);
+
+
+
+        }
+    }
+}
