7da6f8feb630d465b543c9fd356cbefc3808b643
kglim <i7027@naver.com>
2022-03-16 17:35:20 +0900

    ecount class source0316_2

---
+++ FrameWork_Collection/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"D:\temp\test.txt";
+            File.Create(path);
+            bool exist = File.Exists(path);
+            if (exist == true)
+            {
+                Console.WriteLine("{0} 존재합니다", path);
+            }
+            else
+            {
+                Console.WriteLine("{0} 생성되지 않았습니다", path);
+            }
+        }
+    }
+}
