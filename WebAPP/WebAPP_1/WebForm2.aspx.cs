79f6f42e960f6f888a147ba0a88455778f712d1c
kglim <i7027@naver.com>
2022-04-12 17:04:27 +0900

    ecount DB source0412

---
+++ WebAPP/WebAPP_1/WebForm2.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+namespace WebAPP_1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //string source = @"Server=DESKTOP-DD3FU43\SQLEXPRESS;uid=sa;pwd=1004;database=KosaDB";
+            string source = @"Data Source=DESKTOP-DD3FU43\SQLEXPRESS;Initial Catalog=KosaDB;Integrated Security=SSPI;";
+            //1. db connect
+            SqlConnection conn = new SqlConnection(source);
+            conn.Open();
+
+            //2. instruction 
+            String sql = "SELECT * FROM EMP WHERE EMPNO=" + Request["empno"];
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            //3. instruction exe
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            //4. show pages
+
+            Response.Write("<table border=1>");
+
+            while (reader.Read())
+            {
+                Response.Write("<tr>");
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Response.Write("<td>" + reader[i] + "</td>");
+                }
+                Response.Write("</tr>");
+            }
+
+            Response.Write("</table>");
+
+            // 5. db close
+            reader.Close();
+            conn.Close();
+        }
+    }
+}
---
+++ WebAPP/WebAPP_1/WebForm3.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAPP_1
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] keyword = {
+   "Anna"
+  ,"Brittany"
+  ,"Cinderella"
+  ,"Diana"
+  ,"Eva"
+  ,"Fiona"
+  ,"Gunda"
+  ,"Hege"
+  ,"Inga"
+  ,"Johanna"
+  ,"Kitty"
+  ,"Linda"
+  ,"Nina"
+  ,"Ophelia"
+  ,"Petunia"
+  ,"Amanda"
+  ,"Raquel"
+  ,"Cindy"
+  ,"Doris"
+  ,"Eve"
+  ,"Evita"
+  ,"Sunniva"
+  ,"Tove"
+  ,"Unni"
+  ,"Violet"
+  ,"Liza"
+  ,"Elizabeth"
+  ,"Ellen"
+  ,"Wenche"
+  ,"Vicky" };
+
+            string q = Request["word"]; 
+            string hint = "";
+
+            // lookup all hints from array if $q is different from "" 
+           if (q != "")
+            {
+                q = q.ToLower();  //ABC >> abc
+                int len = q.Length; //3
+                foreach (string str in keyword)
+                {  //"Anna"
+                   //out.print(str.substring(0, len));
+
+                    if (str.Substring(0, len).ToLower().Equals(q))
+                    {
+                        if (hint == "")
+                        {
+                            //out.print("data : " + hint);
+                            hint = str;
+                        }
+                        else
+                        {
+                            hint += "." + str;
+                        }
+                    }
+                }
+            }
+           
+            Response.Write(hint);
+        }
+    }
+}
